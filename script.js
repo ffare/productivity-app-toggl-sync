2ee86b449fb3a7f7430b1c8e30ad1949e6c34e12
ffare <faresbenlakdher@gmail.com>
2026-03-16 13:05:26 +0100

    pausing somewhat works

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
 const clockButton = document.querySelector(".clock-button");
 const timeCurrent = clockButton.querySelector(".clock-button__time-current");
-const start = Date.now();
+let start = 0;
+let savedTime = 0;
 
 clockButton.addEventListener("click", () => {
     clockButton.classList.toggle(".clicked");
-    clockButton.setAttribute("data-value", Date.now());
+
+    start = Date.now() - start;    
+    clockButton.setAttribute("data-start", Date.now());
 })
+
 
 // clockButton.addEventListener("click", setInterval);
 let hours = '';
@@ -15,11 +19,13 @@ let seconds = '';
 let milliSeconds = '';
 
 function updateTimer() {
-    timeCurrent.textContent = `${minutes}:${seconds}.${milliSeconds}`;
+    if (clockButton.classList.contains(".clicked")) {
+        timeCurrent.textContent = `${minutes}:${seconds}.${milliSeconds}`;
+    }    
 }
 
 setInterval(() => {
-    delta = Date.now() - start;
+    delta = Date.now() - start + savedTime;
     hours = Math.floor(delta / 3600000).toString().padStart(2, "0");
     minutes = Math.floor((delta % 3600000) / 60000).toString().padStart(2, "0");
     seconds = Math.floor((delta % 60000) / 1000).toString().padStart(2, "0");
